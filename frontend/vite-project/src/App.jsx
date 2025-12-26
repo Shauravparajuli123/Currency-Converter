793f9f040eb8ff067d8409685d3b3c9adc7f187c
Shaurav Parajuli <parajulishaurav8@gmail.com>
2025-12-25 22:36:23 -0500

    First push with some backend errors

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./CurrencyConverter.css";
+
+function App() {
+  const [formData, setFormData] = useState({
+    from: "",
+    to: "",
+    amount: "",
+  });
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState("");
+
+  const currencyCodes = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN",
+"BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTN", "BWP", "BYN", "BZD",
+"CAD", "CDF", "CHF", "CLF", "CLP", "CNH", "CNY", "COP", "CRC", "CUP", "CVE", "CZK",
+"DJF", "DKK", "DOP", "DZD",
+"EGP", "ERN", "ETB", "EUR",
+"FJD", "FKP", "FOK",
+"GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD",
+"HKD", "HNL", "HRK", "HTG", "HUF",
+"IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK",
+"JEP", "JMD", "JOD", "JPY",
+"KES", "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT",
+"LAK", "LBP", "LKR", "LRD", "LSL", "LYD",
+"MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN",
+"NAD", "NGN", "NIO", "NOK", "NPR", "NZD",
+"OMR",
+"PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG",
+"QAR",
+"RON", "RSD", "RUB", "RWF",
+"SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLE", "SOS", "SRD", "SSP", "STN", "SYP", "SZL",
+"THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TVD", "TWD", "TZS",
+"UAH", "UGX", "USD", "UYU", "UZS",
+"VES", "VND", "VUV",
+"WST",
+"XAF", "XCD", "XDR", "XOF", "XPF",
+"YER",
+"ZAR", "ZMW", "ZWL"];
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //Http Request
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/convert",
+        formData
+      );
+      setResult(response?.data);
+      setError("");
+    } catch (error) {
+      setError(
+        "Error",
+        error?.response ? error?.response?.data : error?.message
+      );
+    }
+  };
+
+  return (
+    <div>
+      <section className="hero">
+        <h1>Global Currency Converter</h1>
+        <p>Your go-to solution for real-time currency conversions worldwide.</p>
+      </section>
+      <section className="converter">
+        <form onSubmit={handleSubmit}>
+          <select
+            name="from"
+            value={formData.from}
+            onChange={handleChange}
+            className="input"
+          >
+            <option value="">Select From Currency</option>
+            {currencyCodes.map((code) => (
+              <option key={code} value={code}>
+                {code}
+              </option>
+            ))}
+          </select>
+          <select
+            name="to"
+            value={formData.to}
+            onChange={handleChange}
+            className="input"
+          >
+            <option value="">Select To Currency</option>
+            {currencyCodes.map((code) => (
+              <option key={code} value={code}>
+                {code}
+              </option>
+            ))}
+          </select>
+          <input
+            name="amount"
+            value={formData.amount}
+            onChange={handleChange}
+            placeholder="Amount"
+            type="number"
+            className="input"
+          />
+          <button type="submit" className="submit-btn">
+            Convert
+          </button>
+        </form>
+        {result && (
+          <div className="result">
+            <p>
+              Converted Amount: {result.convertedAmount} {result.target}
+            </p>
+            <p>Conversion Rate: {result.conversionRate}</p>
+          </div>
+        )}
+        {error && <p className="error">Error: {error}</p>}
+      </section>
+      <section className="additional-info">
+        <h2>Why Choose Global Currency Converter?</h2>
+        <p>Detailed explanations on advantages or instructions for use.</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
